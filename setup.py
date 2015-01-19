f1e071a774f3b7d216d172851e4ee9e11f837cf2
Marek Sezemsky <marek.sezemsky@gmail.com>
2015-01-19 23:06:34 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+        name='gitstats',
+        version='0.1',
+        description='Tiny Python tool to generate statistics of a Git repository',
+        keywords='git data',
+        author='https://github.com/hoxu',
+        author_email='',
+        maintainer='https://github.com/marek-sezemsky',
+        maintainer_email='',
+        url='http://github.com/hoxu/gitstats/',
+        dependency_links=[],
+        py_modules=['gitstats'],
+        include_package_data=True,
+        )
+
